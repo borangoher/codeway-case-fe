5c80dbf065c460eb57dd68b3c6df577830f5c9e9
boran <borangoher@proton.me>
2024-11-07 18:49:18 +0300

    feat: Add basic home page

---
+++ src/components/PropertyConfig.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  id: number
+  paramKey: string
+  paramValue: string
+  paramDesc: string
+  createdDate: Date
+}>()
+</script>
+
+<template>
+  <div class="param-line">
+    <p>{{ paramKey }}</p>
+    <p>{{ paramValue }}</p>
+    <p>{{ paramDesc }}</p>
+    <p>{{ createdDate.toString() }}</p>
+    <div class="button-container">
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.param-line {
+  display: grid;
+  grid-template-columns: 1fr 1fr 2fr 1fr 1fr;
+}
+
+.button-container {
+  display: flex;
+  gap: 1rem;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue'
+import PropertyConfig from '../components/PropertyConfig.vue'
 </script>
 
 <template>
   <main>
-    <HelloWorld msg="Hello" />
+    <div class="container">
+      <div class="title-row">
+        <p>Parameter Key</p>
+        <p>Value</p>
+        <p>Description</p>
+        <p>Created Date</p>
+      </div>
+      <PropertyConfig
+        :id="1"
+        paramKey="test"
+        paramValue="test-val"
+        paramDesc="A test parameter"
+        :createdDate="new Date(0)"
+      />
+    </div>
   </main>
 </template>
+
+<style scoped>
+.title-row {
+  display: grid;
+  grid-template-columns: 1fr 1fr 2fr 1fr 1fr;
+  border-bottom: 1px solid red;
+}
+</style>
