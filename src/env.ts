e9993ca0f5899d510241ceac5fbc6de403746414
boran <borangoher@proton.me>
2024-11-18 11:19:15 +0300

    feat: Remove secrets

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,5 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyClwQPDNdY7i93Fz32SQatEkZlMTMfmOww',
-  authDomain: 'codeway-case-be.firebaseapp.com',
-  databaseURL: 'https://codeway-case-be-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'codeway-case-be',
-  storageBucket: 'codeway-case-be.firebasestorage.app',
-  messagingSenderId: '554726911595',
-  appId: '1:554726911595:web:8c3588811956cf24bc4b2e',
-}
+const firebaseConfig = {}
 
-const apiUrl = 'https://codewaydemodeploy.lm.r.appspot.com'
+const apiUrl = ''
 
 export { firebaseConfig, apiUrl }
